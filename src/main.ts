c9eb5ac93a2bd7b57351af36778e4a6d7989ec39
lqd1434 <1434288209@qq.com>
2021-08-18 10:07:36 +0800

    :bug:修改deploy.yml文件

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ import { SocketIoAdapter } from './adapter/redis-socket'
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
 	app.useWebSocketAdapter(new SocketIoAdapter(app))
-	await app.listen(3002)
+	await app.listen(8080)
 }
 bootstrap()
